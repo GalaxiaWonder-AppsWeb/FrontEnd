983ed5a76f6701ef07c34e8687c49255a23d5f8b
L1LZ4Z <joaquinorozco2004@gmail.com>
2025-05-05 20:01:43 -0500

    chore: add configuration set up for i18n

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import i18n from "./i18n.js";
+import './style.css'
+
+createApp(App).use(i18n)
+    .mount('#app')

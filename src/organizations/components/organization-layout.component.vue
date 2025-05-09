a07ee73ce8f4993a56b33e368cacfdaed2092339
Henry Reaño <henry29.2004@hotmail.com>
2025-05-09 18:18:46 -0500

    chore: save a user's organization role to local storage

--- src/organizations/components/organization-layout.component.vue
+++ src/organizations/components/organization-layout.component.vue
@@ -10,11 +10,38 @@
 
 <script>
 import ToolbarComponent from '../../public/components/toolbar-content.component.vue'
+import {organizationMemberService} from "../services/organization-member.service.js";
 
 export default {
   name: 'OrganizationLayout',
+  data(){
+    return {
+      personId: null,
+      organizationId: null
+    }
+  },
   components: {
     ToolbarComponent
+  },
+  methods: {
+    async GetMemberType() {
+      try {
+        const res = await organizationMemberService.getByPersonAndOrgId({
+          personId: this.personId,
+          organizationId: this.organizationId
+        })
+        const user = JSON.parse(localStorage.getItem("user"))
+        console.log("RES:",res)
+        user.activeOrganizationRole = res[0].type
+        localStorage.setItem("user", JSON.stringify(user))
+        console.log("CREDENCIALES DEL USUARIO DENTRO DE UNA ORGANIZACION:",user)
+      } catch (err) {
+        console.error("Error al obtener rol de organización:", err)
+      }
+    }
+  },
+  created() {
+    this.GetMemberType()
   }
 }
 

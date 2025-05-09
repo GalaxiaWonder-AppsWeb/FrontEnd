086a117f9629d2aca089e66621c1ff2619cac79b
Henry Reaño <henry29.2004@hotmail.com>
2025-05-08 19:26:36 -0500

    feat(ep09): add organization item component

---
+++ src/organizations/components/organization-item.component.vue
@@ -0,0 +1,52 @@
+<template>
+  <pv-card class="organization-card">
+    <template #content>
+      <div class="organization-content">
+        <span class="organization-name">{{ organization.legalName }}</span>
+      </div>
+    </template>
+  </pv-card>
+</template>
+
+<script>
+import { Organization } from "../model/organization.entity.js";
+
+export default {
+  name: 'OrganizationItem',
+  props: {
+    organization: {
+      type: Organization,
+      required: true
+    },
+    personId: String
+  },
+  created() {
+    console.log(this.organization)
+  }
+}
+</script>
+
+<style scoped>
+.organization-card {
+  border-radius: 16px;
+  background-color: #f8fbfa;
+  box-shadow: 0 4px 12px rgba(0, 0, 0, 0.08);
+  padding: 1.2rem 1.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.organization-content {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.organization-name {
+  font-size: 1.2rem;
+  font-weight: 700;
+  color: #101020;
+}
+</style>

9bdda08a105cd963384ade62ec246ea35634aa11
Henry Reaño <henry29.2004@hotmail.com>
2025-05-08 19:27:03 -0500

    feat(ep09): add organization list component

---
+++ src/organizations/components/organization-list.component.vue
@@ -0,0 +1,49 @@
+<script>
+import OrganizationItem from "./organization-item.component.vue";
+import {organizationService} from "../services/organization.service.js";
+import {OrganizationAssembler} from "../services/organization.assembler.js";
+
+export default {
+  name: "OrganizationList",
+  components: {OrganizationItem},
+  data() {
+    return {
+      organizations: [],
+      api: organizationService,
+      owner: ''
+    }
+  },
+  methods: {
+    loadOrganizations(){
+      console.log("Owner: ", this.owner)
+      this.api.getByCreatedBy({createdBy: this.owner.id})
+          .then(data => {
+            this.organizations = OrganizationAssembler.toEntitiesFromResponse(data);
+          })
+          .catch(error => {
+            console.log(error);
+            this.errors.push(error);
+            this.organizations = [];
+          })
+      console.log(this.organizations)
+    }
+  },
+  created() {
+    this.owner = JSON.parse(localStorage.getItem("user"))
+    this.loadOrganizations();
+  }
+}
+</script>
+
+<template>
+  <div class="organization-list">
+
+    <div class="organization-items">
+      <OrganizationItem v-for="(item, index) in organizations" :key="index" :organization="item" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>

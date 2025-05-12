256089134ffcb4fa9a668e49358cc892ba0603c5
CodyLionVivo <leon.fabri.vivas@hotmail.com>
2025-05-12 15:10:44 -0500

    feat(ep01): create schedule.entity.js

---
+++ src/projects/model/schedule.entity.js
@@ -0,0 +1,32 @@
+export class Schedule {
+    constructor({ milestones = [] } = {}) {
+        if (!Array.isArray(milestones)) {
+            throw new Error('Milestones must be an array.');
+        }
+        this.milestones = milestones;
+    }
+
+    addMilestone(milestone) {
+        if (!(milestone instanceof Milestone)) {
+            throw new Error('Milestone must be a valid instance of Milestone.');
+        }
+
+        this.milestones.push(milestone);
+    }
+
+    removeMilestone(milestoneId) {
+        if (!(milestoneId instanceof MilestoneId)) {
+            throw new Error('MilestoneId must be a valid instance of MilestoneId.');
+        }
+
+        this.milestones = this.milestones.filter(
+            milestone => milestone.id.value !== milestoneId.value
+        );
+    }
+
+    toJSON() {
+        return {
+            milestones: this.milestones.map(milestone => milestone.toJSON())
+        };
+    }
+}
